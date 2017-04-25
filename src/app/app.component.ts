7bf2d6b35c496a862f5bc178bdbd338b47bfb422
= <prianikovkirill@gmail.com>
2017-04-25 17:30:51 +0300

    end of day 2 to-do list in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,47 @@
 import { Component } from '@angular/core';
 
+class Todo{
+  constructor(  public title: string, 
+                public completed: boolean = false) {}
+}
+const todos: Todo[] = [
+  { 
+    title: 'Learn Angular 2',
+    completed: false
+  },
+  { 
+    title: 'Learn JavaScript',
+    completed: true
+  },
+  { 
+    title: 'Make Application',
+    completed: true
+  }
+];
+
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
-export class AppComponent  { name = 'Kostiantyn'; }
+export class AppComponent  { 
+  title: string = 'Angular 2Do';
+  todos: Todo[] = todos;
+
+  createTask(){
+    let todo: Todo = new Todo(this.newTodoTitle);
+    this.todos.push(todo);
+    this.newTodoTitle = '';
+  }
+
+  taskToggle(todo: Todo){
+    todo.completed = !todo.completed;
+  }
+  deleteTask(todo: Todo){
+    let index: number = this.todos.indexOf(todo);
+    if (index>-1) {
+      this.todos.splice(index, 1);
+    }
+  }
+}
